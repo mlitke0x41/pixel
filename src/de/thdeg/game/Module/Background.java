7329e24166f075225af5021b6d70ef8f50b534bf
Michael Stieglmeier <michael.stieglmeier@stud.th-deg.de>
2022-01-04 21:02:34 +0100

    Import von InternalLedGameThread für spätere Implementierung.

--- src/de/thdeg/game/Module/Background.java
+++ src/de/thdeg/game/Module/Background.java
@@ -1,5 +1,8 @@
 package de.thdeg.game.Module;
 
+import de.thdeg.game.runtime.InternalLedGameThread;
+import java.awt.*;
+
 public class Background extends GameObject{
     //Instanzattribut
     short[] myImage=new short[24*48*3];
