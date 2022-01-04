aac14537a014bbc5e5590b101c286dd0a5ee2d0a
Michael Stieglmeier <michael.stieglmeier@stud.th-deg.de>
2022-01-04 21:35:51 +0100

    Grundlegende Umsetzung von GameFigure.java

--- src/de/thdeg/game/Module/GameFigure.java
+++ src/de/thdeg/game/Module/GameFigure.java
@@ -1,4 +1,34 @@
 package de.thdeg.game.Module;
 
+import java.awt.*;
+
 public abstract class GameFigure extends GameObject {
+    //Instanzattribute
+    private int xpos=0;
+    private int ypos=0;
+
+    //Konstruktor
+    protected GameFigure(int xpos, int ypos, Color color){
+        super(color);
+        this.xpos = xpos;
+        this.ypos = ypos;
+    }
+
+    //Getter
+    public int getXpos(){
+        return(this.xpos);
+    }
+
+    public int getYpos(){
+        return(this.ypos);
+    }
+
+    //Setter
+    private void setXpos(int xpos){
+        this.xpos = xpos;
+    }
+
+    private void setYpos(int ypos){
+        this.ypos = ypos;
+    }
 }
