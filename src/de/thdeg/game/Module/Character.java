c93def34b8903b9e9f94e056090ab8b98718d33c
Michael Stieglmeier <michael.stieglmeier@stud.th-deg.de>
2022-01-04 22:09:32 +0100

    Grundsätzliche Umsetzung von Character.java

--- src/de/thdeg/game/Module/Character.java
+++ src/de/thdeg/game/Module/Character.java
@@ -1,4 +1,18 @@
 package de.thdeg.game.Module;
 
+import java.awt.*;
+
 public class Character extends GameFigure {
+    //Creating the character object
+    private static final Character character = new Character();
+
+    //Constructor
+    private Character(){
+        super(0,0, Color.white);
+    }
+
+    //Getter
+    public static Character getInstance(){
+        return character;
+    }
 }
