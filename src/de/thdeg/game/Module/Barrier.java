49fddbaa202a68d359fcd6af6c23e4ae5f01bad4
Michael Stieglmeier <michael.stieglmeier@stud.th-deg.de>
2022-01-16 10:34:28 +0100

    Es wird nun MINDESTENS eine Lücke erzeugt -> Vermeidung eines sicheren Todes

--- src/de/thdeg/game/Module/Barrier.java
+++ src/de/thdeg/game/Module/Barrier.java
@@ -10,6 +10,7 @@ public class Barrier extends GameFigure{
 
     private ArrayList<Short> positionen;
     private int endPos = 0;
+    private int lücken;
 
     public Barrier(){
         super(1, 1, new short[]{255,0,0});
@@ -29,22 +30,24 @@ public class Barrier extends GameFigure{
     }
 
     public void placeBarrier(short[] barrier) throws InterruptedException {
+        lücken = 0;
         for (int i=0; i<=135; i+=3) {
 
 
             //Zufallszahl, um Lücken in Barrier zu generieren
             Random random = new Random();
-            int value = random.nextInt(8 + 1) + 1;
+            int value = random.nextInt(7 + 1) + 1;
 
-            if(value != 1) {
+            if(value != 1 && !(i==135 && lücken==0)) {
                 short position = (short) ((ypos * 48 + xpos) * 3 + i);
                 positionen.add(position);
                 barrier[position + 0] = getRgbColor()[0];
                 barrier[position + 1] = getRgbColor()[1];
                 barrier[position + 2] = getRgbColor()[2];
-            }
+            } else lücken++;
         }
-        Thread.sleep(50);
+
+        //Thread.sleep(10);
         InternalLedGameThread.showImage(barrier);
     }
 
@@ -79,7 +82,7 @@ public class Barrier extends GameFigure{
 
 
             }
-        Thread.sleep(200);
+        //Thread.sleep(200);
         InternalLedGameThread.showImage(barrier);
         }
         endPos++;
